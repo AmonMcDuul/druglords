d8cde80530e713c3eef408cea3d12cec4731f940
Subtracting <subtracting@gmail.com>
2021-05-28 15:46:05 +0200

    First commit

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import PySimpleGUI as sg
 
-choices = ('Wiet', 'Hash')
+choices = ('Wiet', 'Hash', 'Piemel Test')
 
-left_layout = [  [sg.Text('Drugaloo')],
-            [sg.Listbox(choices, size=(15, len(choices)), key='-DRUG-', enable_events=True)] ]
+left_layout = [[sg.Text('Drugaloo')],
+               [sg.Listbox(choices, size=(15, len(choices)), key='-DRUG-', enable_events=True)]]
 
 right_layout = [[sg.Text('Bugaloo')],
-                [sg.MLine(key='-ML-', size=(40,8))]]
+                [sg.MLine(key='-ML-', size=(40, 8))]]
 
 combined_layout = [[sg.Column(left_layout), sg.Column(right_layout)],
-                    [sg.OK()]]
+                   [sg.OK()]]
 
 window = sg.Window('Druglordzz', combined_layout)
 sg.cprint_set_output_destination(window, '-ML-')
 
-while True:     
+while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
-    if values['-DRUG-']: 
+    if values['-DRUG-']:
         sg.cprint('farts')
 window.close()
