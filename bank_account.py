5891fb9dcb0eaf1d7d15e9096c4b8f912fa74543
Subtracting <subtracting@gmail.com>
2021-05-30 16:25:07 +0200

    bank account

---
+++ bank_account.py
@@ -0,0 +1,18 @@
+import drugs_stats as ds
+from main import balance
+
+
+def update_balance(string, drug, amount):
+    global balance
+    data = ds.get_data()
+    for d in data:
+        if d[0] == drug:
+            if string == 'buy':
+                balance -= d[1] * amount
+            elif string == 'sell':
+                balance += d[1] * amount
+    return balance
+
+
+def get_balance():
+    return balance
