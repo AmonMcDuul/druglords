d1caf7ce71566baa3e72ffd4b18f75866ed0444b
AmonMcDuul <joranomic@hotmail.com>
2021-05-31 20:00:21 +0200

    shopidoodledoo

--- shop.py
+++ shop.py
@@ -19,13 +19,13 @@ knight_icon = 'img\\knightarmor.png'
 
 def shop_view():
     shop_layout = [[sg.Text('Get to da Shoppa')],
-                   [sg.SimpleButton('', image_filename=weapon_shop_icon, image_size=(
+                   [sg.SimpleButton('1000', image_filename=weapon_shop_icon, image_size=(
                        100, 100), image_subsample=2, border_width=1, key='-WEAPON-'), sg.SimpleButton('', image_filename=armor_shop_icon, image_size=(
                            100, 100), image_subsample=2, border_width=1, key='-ARMOR-'), sg.SimpleButton('', image_filename=fart_icon, image_size=(
                                100, 100), image_subsample=2, border_width=1, key='-FART-')]]
-    shop_window = sg.Window('Shoppa', shop_layout)
 
     while True:
+        shop_window = sg.Window('Shoppa', shop_layout)
         event, values = shop_window.read()
         if event == sg.WIN_CLOSED:
             break
@@ -37,52 +37,111 @@ def shop_view():
             break
         if event == '-FART-':
             sg.popup('You aint no buying no farts dawg!')
+            break
     shop_window.close()
 
 
 def weapon_view():
-    weapon_shop_layout = [[sg.Text('Weapon shop')],
-                          [sg.SimpleButton('', image_filename=knife_icon, image_size=(
-                              100, 100), image_subsample=2, border_width=1, key='-KNIFE-'), sg.SimpleButton('', image_filename=gun_icon, image_size=(
-                                  100, 100), image_subsample=2, border_width=1, key='-GUN-'), sg.SimpleButton('', image_filename=ak_icon, image_size=(
-                                      100, 100), image_subsample=2, border_width=1, key='-AK-')]]
-    weapon_shop_window = sg.Window('Shoppa', weapon_shop_layout)
+    knife_layout = [[sg.Text('Knife')],
+                    [sg.SimpleButton('', image_filename=knife_icon, image_size=(
+                        100, 100), image_subsample=2, border_width=1, key='-KNIFE-')],
+                    [sg.Text('1000,00', text_color=ba.shop_price_colour(1000))]]
+
+    gun_layout = [[sg.Text('Gun')],
+                  [sg.SimpleButton('', image_filename=gun_icon, image_size=(
+                      100, 100), image_subsample=2, border_width=1, key='-GUN-')],
+                  [sg.Text('5000,00', text_color=ba.shop_price_colour(5000))]]
+
+    ak_layout = [[sg.Text('AK47')],
+                 [sg.SimpleButton('', image_filename=ak_icon, image_size=(
+                     100, 100), image_subsample=2, border_width=1, key='-AK-')],
+                 [sg.Text('10000,00', text_color=ba.shop_price_colour(10000))]]
+
+    weapon_shop_layout = [
+        [sg.Col(knife_layout), sg.Col(gun_layout), sg.Col(ak_layout)]]
 
     while True:
+        weapon_shop_window = sg.Window('Weapon shop', weapon_shop_layout)
         event, values = weapon_shop_window.read()
         if event == sg.WIN_CLOSED:
             break
         if event == '-KNIFE-':
-            ch.weapon_bought('Knife')
+            if can_buy(1000):
+                ch.weapon_bought('Knife')
+                ba.update_balance_shop(1000)
+            else:
+                sg.popup('You aint got no money fool!')
             break
         if event == '-GUN-':
-            ch.weapon_bought('Gun')
+            if can_buy(5000):
+                ch.weapon_bought('Gun')
+                ba.update_balance_shop(5000)
+            else:
+                sg.popup('You aint got no money fool!')
             break
         if event == '-AK-':
-            ch.weapon_bought('Ak47')
+            if can_buy(10000):
+                ch.weapon_bought('Ak47')
+                ba.update_balance_shop(10000)
+            else:
+                sg.popup('You aint got no money fool!')
             break
     weapon_shop_window.close()
+    shop_view()
 
 
 def armor_view():
-    armor_shop_layout = [[sg.Text('Armor shop')],
-                         [sg.SimpleButton('', image_filename=vest_icon, image_size=(
-                             100, 100), image_subsample=2, border_width=1, key='-VEST-'), sg.SimpleButton('', image_filename=armor_icon, image_size=(
-                                 100, 100), image_subsample=2, border_width=1, key='-ARMOR-'), sg.SimpleButton('', image_filename=knight_icon, image_size=(
-                                     100, 100), image_subsample=2, border_width=1, key='-KNIGHT-')]]
-    armor_shop_window = sg.Window('Shoppa', armor_shop_layout)
+    vest_layout = [[sg.Text('Vest')],
+                   [sg.SimpleButton('', image_filename=vest_icon, image_size=(
+                       100, 100), image_subsample=2, border_width=1, key='-VEST-')],
+                   [sg.Text('1000,00', text_color=ba.shop_price_colour(1000))]]
+
+    armor_layout = [[sg.Text('Kevlar')],
+                    [sg.SimpleButton('', image_filename=armor_icon, image_size=(
+                        100, 100), image_subsample=2, border_width=1, key='-ARMOR-')],
+                    [sg.Text('5000,00', text_color=ba.shop_price_colour(5000))]]
+
+    knight_layout = [[sg.Text('Knight armor')],
+                     [sg.SimpleButton('', image_filename=knight_icon, image_size=(
+                         100, 100), image_subsample=2, border_width=1, key='-KNIGHT-')],
+                     [sg.Text('10000,00', text_color=ba.shop_price_colour(10000))]]
+
+    armor_shop_layout = [
+        [sg.Col(vest_layout), sg.Col(armor_layout), sg.Col(knight_layout)]]
 
     while True:
+        armor_shop_window = sg.Window('Shoppa', armor_shop_layout)
         event, values = armor_shop_window.read()
         if event == sg.WIN_CLOSED:
             break
         if event == '-VEST-':
-            ch.armor_bought('Vest')
+            if can_buy(1000):
+                ch.armor_bought('Vest')
+                ba.update_balance_shop(1000)
+            else:
+                sg.popup('You aint got no money fool!')
             break
         if event == '-ARMOR-':
-            ch.armor_bought('Kevlar')
+            if can_buy(5000):
+                ch.armor_bought('Kevlar')
+                ba.update_balance_shop(5000)
+            else:
+                sg.popup('You aint got no money fool!')
             break
         if event == '-KNIGHT-':
-            ch.armor_bought('Knight armor')
+            if can_buy(10000):
+                ch.armor_bought('Knight armor')
+                ba.update_balance_shop(10000)
+            else:
+                sg.popup('You aint got no money fool!')
             break
     armor_shop_window.close()
+    shop_view()
+
+
+def can_buy(n):
+    balance = ba.get_balance()
+    if balance - n >= 0:
+        return True
+    else:
+        return False
